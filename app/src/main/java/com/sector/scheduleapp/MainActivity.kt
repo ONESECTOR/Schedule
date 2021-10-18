4b6df2acba5be4af9a2d53b892ee211177e56688
sector <75115941+onesector@users.noreply.github.com>
2021-10-18 18:41:28 +0300

    added view binding to all project and MainActivity

--- app/src/main/java/com/sector/scheduleapp/MainActivity.kt
+++ app/src/main/java/com/sector/scheduleapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.sector.scheduleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sector.scheduleapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
